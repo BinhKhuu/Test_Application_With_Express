e08869e53ccce1c5e989e79c6abd225b16a5a967
binhkhuu <binhkhuu90@gmail.com>
2022-06-28 15:07:06 +0800

    add app screen

--- client/react-crud/src/App.js
+++ client/react-crud/src/App.js
@@ -2,11 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      UGH
-    </div>
-  );
+  return <div role="App" className="App">UGH</div>;
 }
 
 export default App;
--- client/react-crud/src/__tests__/App.test.js
+++ client/react-crud/src/__tests__/App.test.js
@@ -1,15 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 describe('App', () => {
   it('Renders the App Component', () => {
-    render(<App></App>);
-    expect(screen.getByText('UGH')).toBeInTheDocument();
+    render(<App/>);
+    expect(screen.getByRole("App")).toBeInTheDocument();
   });
 });
